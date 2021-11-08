d07ccbb40318bc3557878e33cb6523da484b3000
Lan Nguyen <46945964+HaseNguyen@users.noreply.github.com>
2021-11-08 16:18:28 +0900

    push
    push

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿function saySomething(phrase){
+    console.log("You said:" + phrase)
+
+}
+
+function getPhraseLength(phrase){
+    var l = phrase.length;
+    return l;
+}
+
+var thisLength = getPhraseLength("this is a slightly longer sentence ");
+
+console.log(thisLength);
